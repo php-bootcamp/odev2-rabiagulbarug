978dd3cc97bd7d7472142ddb7bc4ec52533188c2
Rabia Gül Baruğ <66168375+rabiagulbarug@users.noreply.github.com>
2021-02-03 16:22:29 +0300

    Update app.php

--- app.php
+++ app.php
@@ -5,6 +5,37 @@ $weight = 55; // kilo (kg)
 $height = 170; // boy (cm)
 $age = 25; // yaş (sene)
 
+$hesapla = 0;
+		if ($sex=="male"){	
+		$hesapla = (10*$weight)+(6.25*$height) - (5*$age)+5;
+		echo $hesapla ;
+      //1492.5 sonucunu döndürür
+		}
+	else if ($sex == "female") {
+		$hesapla = (10*$weight)+ (6.25*$height)-(5*$age) -161;
+		echo $hesapla;    
+}   
+?>
+
+<?php
+
+ $sex = "female";
+ $weight = 60;
+ $height = 170 ;
+ $age = 35 ;
+
+	$hesapla = 0;
+		if ($sex=="male"){	
+		$hesapla = (10*$weight)+(6.25*$height) - (5*$age)+5;
+		echo $hesapla ;
+		}
+	else if ($sex == "female") {
+		$hesapla = (10*$weight)+ (6.25*$height)-(5*$age) -161;
+		echo $hesapla;    
+} // Ekrana 1326.5 yazması gerekiyor.
+
+?>
+
 /**
  * Bu ödevde günlük kalori hesaplaması yapacağız.
  * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
